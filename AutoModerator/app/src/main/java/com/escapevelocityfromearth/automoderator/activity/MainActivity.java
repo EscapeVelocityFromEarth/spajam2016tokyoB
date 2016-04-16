9481777ae41f5715aec8bdda9e7df6a3e14cbc75
Akio Kondo <kojiran@gmail.com>
2016-04-16 20:25:42 +0900

    画面とか追加

--- AutoModerator/app/src/main/java/com/escapevelocityfromearth/automoderator/activity/MainActivity.java
+++ AutoModerator/app/src/main/java/com/escapevelocityfromearth/automoderator/activity/MainActivity.java
@@ -1,21 +1,24 @@
 package com.escapevelocityfromearth.automoderator.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.escapevelocityfromearth.automoderator.R;
 import com.escapevelocityfromearth.automoderator.service.VoiceAnalysisService;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener{
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     TextView text;
     Button startButton;
     Button stopButton;
+    Spinner userTypeSpinner;
+    Button go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,22 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 
         stopButton = (Button) findViewById(R.id.stop);
         stopButton.setOnClickListener(this);
+
+        userTypeSpinner = (Spinner) findViewById(R.id.spinner_user_type);
+
+        go = (Button) findViewById(R.id.button_next);
+        go.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                if (userTypeSpinner.getSelectedItemPosition()==0) {
+                    intent = new Intent(MainActivity.this, AdminActivity.class);
+                } else {
+                    intent = new Intent(MainActivity.this, MemberActivity.class);
+                }
+                startActivity(intent);
+            }
+        });
 
     }
 
