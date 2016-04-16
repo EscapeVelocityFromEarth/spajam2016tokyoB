fd100947b5c89c903cdace617f2f3ce4fb4f1e67
一馬三竹 <mitake@kazumamitake-no-MacBook-Pro.local>
2016-04-16 16:28:02 +0900

    change project name

---
+++ AutoModerator/app/src/main/java/com/escapevelocityfromearth/automoderator/util/Const.java
@@ -0,0 +1,9 @@
+package com.escapevelocityfromearth.automoderator.util;
+
+
+public class Const {
+
+    public static final String SLACK_API_KEY = "xoxp-32785354118-33534119074-35265581346-14ee504a2e";
+    public static final String HACKATHON_CHANNEL = "https://hooks.slack.com/services/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";
+
+}
