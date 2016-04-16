dee24f66f7ff82f0c41b9834b411174f7cdb1fc6
kuwabaradaichi <daichi3189@gmail.com>
2016-04-16 22:23:31 +0900

    音声認識テキストの取得（連続）再push

--- AutoModerator/app/src/main/java/com/escapevelocityfromearth/automoderator/service/VoiceAnalysisService.java
+++ AutoModerator/app/src/main/java/com/escapevelocityfromearth/automoderator/service/VoiceAnalysisService.java
@@ -7,6 +7,7 @@ import android.os.IBinder;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.widget.Toast;
 
 import com.escapevelocityfromearth.automoderator.util.Const;
 import com.escapevelocityfromearth.automoderator.util.L;
@@ -26,6 +27,13 @@ public class VoiceAnalysisService extends Service {
     public void onCreate() {
         L.outputMethodName();
 
+        startListener();
+        sender = new MessageSender(this);
+        sender.sendMessage(System.currentTimeMillis(), Const.CREATE_RECORD_USER, Const.CREATE_RECORD_STRAT);
+
+    }
+
+    private void startListener() {
         if (mSpeechRecognizer == null) {
             mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
             mSpeechRecognizer.setRecognitionListener(mRecognitionListener);
@@ -36,9 +44,6 @@ public class VoiceAnalysisService extends Service {
 
         intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
         mSpeechRecognizer.startListening(intent);
-
-        sender = new MessageSender(this);
-        sender.sendMessage(System.currentTimeMillis(), Const.CREATE_RECORD_USER, Const.CREATE_RECORD_STRAT);
     }
 
     @Override
@@ -63,9 +68,24 @@ public class VoiceAnalysisService extends Service {
 
     private RecognitionListener mRecognitionListener = new RecognitionListener() {
 
+        private Boolean mIsListening = false;
+
+        private void setLiteningFlag(boolean isListening) {
+            synchronized (mIsListening) {
+                mIsListening = isListening;
+            }
+        }
+
+        private boolean getLiteningFlag() {
+            synchronized (mIsListening) {
+                return mIsListening;
+            }
+        }
+
         @Override
         public void onReadyForSpeech(Bundle bundle) {
             L.outputMethodName();
+            setLiteningFlag(true);
         }
 
         @Override
@@ -93,25 +113,34 @@ public class VoiceAnalysisService extends Service {
 
         @Override
         public void onError(int i) {
+            if (!getLiteningFlag()) {
+                return;
+            }
             L.outputMethodName();
             L.d("Error id = " + i);
             switch (i) {
                 case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
                 case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
                 case SpeechRecognizer.ERROR_NO_MATCH:
-                    // TODO もう一度トライ
+                    mSpeechRecognizer.cancel();
+                    startListener();
+                    // もう一度トライ
                     break;
                 case SpeechRecognizer.ERROR_AUDIO:
                 case SpeechRecognizer.ERROR_CLIENT:
                 case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
-                    // TODO キャンセルしてもう一度
+                    // キャンセルしてもう一度
+                    mSpeechRecognizer.cancel();
+                    startListener();
                     break;
                 case SpeechRecognizer.ERROR_NETWORK:
                 case SpeechRecognizer.ERROR_SERVER:
                 case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
-                    // TODO 設定確認必須
+                    // 設定確認必須
+                    Toast.makeText(getApplicationContext(),"ネット接続もしくはパーミッションを確認して下さい",Toast.LENGTH_SHORT).show();
                     break;
             }
+            setLiteningFlag(false);
         }
 
         @Override
@@ -122,6 +151,9 @@ public class VoiceAnalysisService extends Service {
             L.d("get Recognize data = " + arrayList.get(0));
 
             sendTextData(arrayList.get(0));
+            mSpeechRecognizer.cancel();
+            startListener();
+            setLiteningFlag(false);
         }
 
         @Override
