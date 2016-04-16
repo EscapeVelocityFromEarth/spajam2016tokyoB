5d87a1d249744dae1205518f914f9cb9d1f38db8
kuwabaradaichi <daichi3189@gmail.com>
2016-04-16 20:23:07 +0900

    音声認識テキスト取得（一回のみ）

--- AutoModerator/app/src/main/java/com/escapevelocityfromearth/automoderator/service/VoiceAnalysisService.java
+++ AutoModerator/app/src/main/java/com/escapevelocityfromearth/automoderator/service/VoiceAnalysisService.java
@@ -2,27 +2,42 @@ package com.escapevelocityfromearth.automoderator.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
-import android.util.Log;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 
-import com.escapevelocityfromearth.automoderator.util.Const;
+import com.escapevelocityfromearth.automoderator.util.L;
+
+import java.util.ArrayList;
 
 public class VoiceAnalysisService extends Service {
 
-    private static final boolean DBG = Const.DBG;
-    private static final String TAG = "VoiceAnalysisService";
+    private SpeechRecognizer mSpeechRecognizer;
 
     public VoiceAnalysisService() {
     }
 
     @Override
     public void onCreate() {
-        if(DBG) Log.d(TAG, "service onCreate");
+        L.outputMethodName();
+
+        if (mSpeechRecognizer == null) {
+            mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+            mSpeechRecognizer.setRecognitionListener(mRecognitionListener);
+        }
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        mSpeechRecognizer.startListening(intent);
     }
 
     @Override
     public void onDestroy() {
-        if(DBG) Log.d(TAG, "service onDestroy");
+        L.outputMethodName();
     }
 
     @Override
@@ -30,4 +45,87 @@ public class VoiceAnalysisService extends Service {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    // ここに音声認識のテキストデータが入ります。
+    private void sendTextData(String sendData) {
+
+    }
+
+    private RecognitionListener mRecognitionListener = new RecognitionListener() {
+
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+            L.outputMethodName();
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            L.outputMethodName();
+
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+            L.outputMethodName();
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            L.outputMethodName();
+
+        }
+
+        @Override
+        public void onError(int i) {
+            L.outputMethodName();
+            L.d("Error id = " + i);
+            switch (i) {
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    // TODO もう一度トライ
+                    break;
+                case SpeechRecognizer.ERROR_AUDIO:
+                case SpeechRecognizer.ERROR_CLIENT:
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    // TODO キャンセルしてもう一度
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                case SpeechRecognizer.ERROR_SERVER:
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    // TODO 設定確認必須
+                    break;
+            }
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+            L.outputMethodName();
+
+            ArrayList<String> arrayList = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            L.d("get Recognize data = " + arrayList.get(0));
+
+            sendTextData(arrayList.get(0));
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+            L.outputMethodName();
+
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+            L.outputMethodName();
+
+        }
+
+    };
+
 }
